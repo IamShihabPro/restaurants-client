66c5ab8691645c95f5357fa6cec963044eb88899
IamShihabPro <mshihab.pro@gmail.com>
2024-01-08 20:49:46 +0600

    order cart item

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,6 @@
-// Table.jsx
 import React from 'react';
+import { FaTrash } from "react-icons/fa";
+
 
 const Table = ({ item, handleIncreaseQuantity, handleDecreaseQuantity }) => {
     return (
@@ -8,7 +9,7 @@ const Table = ({ item, handleIncreaseQuantity, handleDecreaseQuantity }) => {
                 <td className="py-4 px-6">
                     <div className="flex items-center gap-3">
                         <div className="avatar">
-                            <div className="mask mask-squircle w-12 h-12">
+                            <div className="mask w-14 h-14">
                                 <img src={item.image} alt="Avatar Tailwind CSS Component" className="w-full h-full object-cover" />
                             </div>
                         </div>
@@ -17,29 +18,35 @@ const Table = ({ item, handleIncreaseQuantity, handleDecreaseQuantity }) => {
                         </div>
                     </div>
                 </td>
-                <td className="py-4 px-6">
+                <td className="py-4 px-6 uppercase font-semibold">
                     {item.category}
                 </td>
-                <td className="py-4 px-6 whitespace-no-wrap">
+                <td className="py-4 px-6 whitespace-no-wrap flex items-center justify-center gap-2">
                     <button
-                        className="bg-green-500 text-white px-4 py-2 rounded-full mr-2"
+                        className="bg-green-500 text-white px-4 py-1 mr-2"
                         onClick={() => handleIncreaseQuantity(item._id)}
                     >
                         +
                     </button>
                     <span className="text-lg font-bold">{item.quantity || 1}</span>
                     <button
-                        className="bg-red-500 text-white px-4 py-2 rounded-full ml-2"
+                        className="bg-red-500 text-white px-4 py-1 ml-2 btn-outline"
                         onClick={() => handleDecreaseQuantity(item._id)}
                     >
                         -
                     </button>
                 </td>
-                <td className="py-4 px-6">
+
+                {/* <td className="py-4 px-6">
                     ${item.price.toFixed(2)}
-                </td>
-                <td className="py-4 px-6">
+                </td> */}
+
+                <td className="py-4 px-6 font-bold">
                     ${item.totalPrice ? item.totalPrice.toFixed(2) : '0.00'}
+                </td>
+
+                <td>
+                    <button className='btn rounded-none bg-red-600 text-white btn-sm hover:bg-orange-600'> <FaTrash /> </button>
                 </td>
             </tr>
         </>
--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -51,18 +51,21 @@ const Order = () => {
     };
 
     return (
-        <div className='container mx-auto mt-28'>
+        <div className='container mx-auto mt-28 font-serif'>
             {
                 user ?
-                    <div className="overflow-x-auto">
+                   <>
+                   <h1 className='text-center font-bold text-2xl'>All Cart Item</h1>
+                   <div className="overflow-x-auto px-4 py-4">
                         <table className="min-w-full bg-white border border-gray-300 rounded mx-auto">
-                            <thead className="bg-gray-200">
+                            <thead className="bg-sky-500 text-white">
                                 <tr className="text-center">
                                     <th className="py-2 px-4">Name</th>
                                     <th className="py-2 px-4">Category</th>
                                     <th className="py-2 px-4">Quantity</th>
-                                    <th className="py-2 px-4">Price</th>
+                                    {/* <th className="py-2 px-4">Price</th> */}
                                     <th className="py-2 px-4">Total Price</th>
+                                    <th className="py-2 px-4">Action</th>
                                 </tr>
                             </thead>
                             <tbody className="text-center">
@@ -75,18 +78,20 @@ const Order = () => {
                                     />
                                 ))}
                             </tbody>
-                            <tfoot className="bg-gray-200">
+                            <tfoot className="bg-green-600 text-white text-lg font-bold">
                                 <tr>
-                                    <td colSpan="3" className="py-2 px-4 text-right font-bold">
+                                    <td colSpan="2" className="py-2 px-4 text-right font-bold">
                                         Total:
                                     </td>
-                                    <td colSpan="2" className="py-2 px-4 border-t border-gray-300">
+                                    <td colSpan="3" className="py-2 px-4 border-t border-gray-300">
                                         ${calculateOverallTotal().toFixed(2)}
                                     </td>
                                 </tr>
                             </tfoot>
                         </table>
                     </div>
+                   
+                   </>
                     :
                     <h1 className='text-2xl font-bold font-serif text-center mt-40'> No order food. <br /> Please login for order food. </h1>
             }
