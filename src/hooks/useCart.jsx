60965485dd511b0783cfa34966adbf9a8e30f809
IamShihabPro <mshihab.pro@gmail.com>
2024-01-15 03:27:14 +0600

    jwt

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,18 +1,37 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react'
 import { AuthContext } from '../Provider/AuthProvider'
+import useAxiosSecure from './useAxiosSecure'
 
-const useCart = () =>{
-    const {user} = useContext(AuthContext)
+const useCart = email =>{
+    const {user, loading} = useContext(AuthContext)
+    const token = localStorage.getItem('access-token')
 
-    const { data: cart=[], refetch } = useQuery({
+    const [axiosSecure] = useAxiosSecure()
+
+    const { isLoading, isError, data: cart = [], error, refetch } = useQuery({
         queryKey: ['carts', user?.email],
+        enabled: !loading,
 
-        queryFn: async () => {
-            const res = await fetch(`${import.meta.env.VITE_API_URL}/carts?email=${user?.email}`)
+        queryFn: async() =>{
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/carts?email=${user?.email}`,{
+                headers: {
+                    authorization: `bearer ${token}`
+                }
+            })
+            // console.log(res);
             return res.json()
-        }
+        },
+
+        // queryFn: async () => {
+        //     const res = await axiosSecure(`/carts?email=${user?.email}`)
+        //     console.log('res from axios', res)
+        //     return res.data;
+        // },
+
+
       })
-      return [cart, refetch]
+      return [cart, refetch, isLoading]
+    
 }
-export default useCart;
+export default useCart
